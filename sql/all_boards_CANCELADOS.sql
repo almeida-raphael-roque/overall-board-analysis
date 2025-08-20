2cab280baf690ee5e2158c2112f469866e544818
roque <almeida.raphaelroque@gmail.com>
2025-08-20 11:32:33 -0300

    real_project_base

--- sql/all_boards_CANCELADOS.sql
+++ sql/all_boards_CANCELADOS.sql
@@ -1,152 +1,237 @@
-SELECT DISTINCT
-    coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
-    coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
-    coalesce(iv.id,it.id,itt.id) as "id_placa",
-    coalesce(irsc.ID_VEHICLE) as "id_veiculo",
-    coalesce(irsct.ID_TRAILER) as "id_carroceria",
-    ir.id as "Matricula",
-    irs.id as "Conjunto",
-    cata.fantasia as "Unidade",
-    iss.description as "Status",
-    cat.nome as "Cliente",
-    irs.data_registration  as "Data",
-    cast(irs.date_cancellation as date) as "data_cancelamento",
-    irs.user_name_cancellation as "usuario_cancelamento",
-    current_date AS "data_filtro",
-    'Segtruck' as empresa
-    
-from silver.insurance_registration ir
-        left outer join silver.insurance_reg_set irs on irs.parent = ir.id
-        left outer join silver.cliente clie on clie.codigo = ir.CUSTOMER_ID
-        left outer join silver.catalogo cat on cat.cnpj_cpf = clie.cnpj_cpf
-        left outer join silver.representante r on r.codigo = irs.id_unity
-        left outer join silver.catalogo cata on cata.cnpj_cpf = r.cnpj_cpf
-        left outer join silver.insurance_status iss on iss.id = irs.id_status
-        left outer join silver.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
-        left outer join silver.INSURANCE_VEHICLE iv on iv.ID = irsc.ID_VEHICLE
-        left outer join silver.TIPO_VEICULO tv on tv.CODIGO = iv.CODE_TYPE_VEHICLE 
-        left outer join silver.INSURANCE_REG_SET_COV_TRAILER irsct on irsct.PARENT = irsc.ID
-        left outer join silver.INSURANCE_TRAILER it on it.ID = irsct.ID_TRAILER
-        left outer join silver.insurance_trailer itt on itt.id = irsc.ID_TRAILER
-        left outer join (
-        select distinct 
-            irs.id as conjunto,
-            wb.user_name as suporte
-    
-            from silver.insurance_reg_set irs 
-                inner join silver.web_user wb on wb.id = irs.id_user_support
-            ) as user_name on user_name.conjunto = irs.id
-            
+WITH CTE_segtruck AS (
+    SELECT
+        coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
+        coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
+        coalesce(iv.id,it.id,itt.id) as "id_placa",
+        coalesce(irsc.ID_VEHICLE) as "id_veiculo",
+        coalesce(irsct.ID_TRAILER) as "id_carroceria",
+        ir.id as "Matricula",
+        irs.id as "Conjunto",
+        cata.fantasia as "Unidade",
+        iss.description as "Status",
+        cat.nome as "Cliente",
+        irs.data_registration  as "Data",
+        cast(irs.date_cancellation as date) as "data_cancelamento",
+        irs.user_name_cancellation as "usuario_cancelamento",
+        current_date AS "data_filtro",
+        'Segtruck' as empresa,
+        irh.data_registration as "data_registro_historico",
+        -- irh.note as "Migração conferencia",
+        CASE
+            WHEN irh.note LIKE '%MIGRAÇÃO%' THEN 'SIM'
+            ELSE 'NAO'
+        END as "migracao",
+        ROW_NUMBER() OVER (PARTITION BY coalesce(iv.BOARD,it.BOARD,itt.BOARD) ORDER BY irs.data_registration DESC) as rn,
+        cast(irs.date_replaced as date) as "data_substituicao"
+    FROM silver.insurance_registration ir
+        LEFT JOIN silver.insurance_reg_set irs ON irs.parent = ir.id
+        LEFT JOIN silver.cliente clie ON clie.codigo = ir.CUSTOMER_ID
+        LEFT JOIN silver.catalogo cat ON cat.cnpj_cpf = clie.cnpj_cpf
+        LEFT JOIN silver.representante r ON r.codigo = irs.id_unity
+        LEFT JOIN silver.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+        LEFT JOIN silver.insurance_status iss ON iss.id = irs.id_status
+        LEFT JOIN silver.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
+        LEFT JOIN silver.INSURANCE_VEHICLE iv ON iv.ID = irsc.ID_VEHICLE
+        LEFT JOIN silver.TIPO_VEICULO tv ON tv.CODIGO = iv.CODE_TYPE_VEHICLE 
+        LEFT JOIN silver.INSURANCE_REG_SET_COV_TRAILER irsct ON irsct.PARENT = irsc.ID
+        LEFT JOIN silver.INSURANCE_TRAILER it ON it.ID = irsct.ID_TRAILER
+        LEFT JOIN silver.insurance_trailer itt ON itt.id = irsc.ID_TRAILER
+        LEFT JOIN silver.insurance_reg_historic irh ON irh.id_set = irs.id
+        LEFT JOIN (
+            SELECT DISTINCT irs.id as conjunto, wb.user_name as suporte
+            FROM silver.insurance_reg_set irs 
+            INNER JOIN silver.web_user wb ON wb.id = irs.id_user_support
+        ) as user_name ON user_name.conjunto = irs.id
+    WHERE date_cancellation IS NOT NULL
+        AND coalesce(iv.BOARD,it.BOARD,itt.BOARD) IS NOT NULL
+        AND coalesce(iv.chassi,it.chassi,itt.chassi) IS NOT NULL
+),
 
-where date_cancellation is not null
-and (
-    coalesce(iv.BOARD,it.BOARD,itt.BOARD) is not null
-    and coalesce(iv.chassi,it.chassi,itt.chassi) is not null
+CTE_stcoop AS (
+    SELECT
+        coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
+        coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
+        coalesce(iv.id,it.id,itt.id) as "id_placa",
+        coalesce(irsc.ID_VEHICLE) as "id_veiculo",
+        coalesce(irsct.ID_TRAILER) as "id_carroceria",
+        ir.id as "Matricula",
+        irs.id as "Conjunto",
+        CASE
+            WHEN cata.nome LIKE '%FTR%' THEN cata.nome
+            ELSE cata.FANTASIA
+        END as "Unidade",
+        iss.description as "Status",
+        cat.nome as "Cliente",
+        irs.data_registration  as "Data",
+        cast(irs.date_cancellation as date) as "data_cancelamento",
+        irs.user_name_cancellation as "usuario_cancelamento",
+        current_date AS "data_filtro",
+        'Stcoop' as empresa,
+        irh.data_registration as "data_registro_historico",
+        -- irh.note as "Migração conferencia",
+        CASE
+            WHEN irh.note LIKE '%MIGRAÇÃO%' THEN 'SIM'
+            ELSE 'NAO'
+        END as "migracao",
+        ROW_NUMBER() OVER (PARTITION BY coalesce(iv.BOARD,it.BOARD,itt.BOARD) ORDER BY irs.data_registration DESC) as rn,
+        cast(irs.date_replaced as date) as "data_substituicao"
+    FROM stcoop.insurance_registration ir
+        LEFT JOIN stcoop.insurance_reg_set irs ON irs.parent = ir.id
+        LEFT JOIN stcoop.cliente clie ON clie.codigo = ir.CUSTOMER_ID
+        LEFT JOIN stcoop.catalogo cat ON cat.cnpj_cpf = clie.cnpj_cpf
+        LEFT JOIN stcoop.representante r ON r.codigo = irs.id_unity
+        LEFT JOIN stcoop.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+        LEFT JOIN stcoop.insurance_status iss ON iss.id = irs.id_status
+        LEFT JOIN stcoop.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
+        LEFT JOIN stcoop.INSURANCE_VEHICLE iv ON iv.ID = irsc.ID_VEHICLE
+        LEFT JOIN stcoop.TIPO_VEICULO tv ON tv.CODIGO = iv.CODE_TYPE_VEHICLE 
+        LEFT JOIN stcoop.INSURANCE_REG_SET_COV_TRAILER irsct ON irsct.PARENT = irsc.ID
+        LEFT JOIN stcoop.INSURANCE_TRAILER it ON it.ID = irsct.ID_TRAILER
+        LEFT JOIN stcoop.insurance_trailer itt ON itt.id = irsc.ID_TRAILER
+        LEFT JOIN stcoop.insurance_reg_historic irh ON irh.id_set = irs.id
+        LEFT JOIN (
+            SELECT DISTINCT irs.id as conjunto, wb.user_name as suporte
+            FROM stcoop.insurance_reg_set irs 
+            INNER JOIN stcoop.web_user wb ON wb.id = irs.id_user_support
+        ) as user_name ON user_name.conjunto = irs.id
+    WHERE date_cancellation IS NOT NULL
+        AND coalesce(iv.BOARD,it.BOARD,itt.BOARD) IS NOT NULL
+        AND coalesce(iv.chassi,it.chassi,itt.chassi) IS NOT NULL
+),
+
+CTE_viavante AS (
+    SELECT
+        coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
+        coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
+        coalesce(iv.id,it.id,itt.id) as "id_placa",
+        coalesce(irsc.ID_VEHICLE) as "id_veiculo",
+        coalesce(irsct.ID_TRAILER) as "id_carroceria",
+        ir.id as "Matricula",
+        irs.id as "Conjunto",
+        CASE
+            WHEN cata.nome LIKE '%FTR%' THEN cata.nome
+            ELSE cata.FANTASIA
+        END as "Unidade",
+        iss.description as "Status",
+        cat.nome as "Cliente",
+        irs.data_registration  as "Data",
+        cast(irs.date_cancellation as date) as "data_cancelamento",
+        irs.user_name_cancellation as "usuario_cancelamento",
+        current_date AS "data_filtro",
+        'Viavante' as empresa,
+        irh.data_registration as "data_registro_historico",
+        -- irh.note as "Migração conferencia",
+        CASE
+            WHEN irh.note LIKE '%MIGRAÇÃO%' THEN 'SIM'
+            ELSE 'NAO'
+        END as "migracao",
+        ROW_NUMBER() OVER (PARTITION BY coalesce(iv.BOARD,it.BOARD,itt.BOARD) ORDER BY irs.data_registration DESC) as rn,
+        cast(irs.date_replaced as date) as "data_substituicao"
+    FROM viavante.insurance_registration ir
+        -- joins como estavam
+        LEFT JOIN viavante.insurance_reg_set irs ON irs.parent = ir.id
+        LEFT JOIN viavante.cliente clie ON clie.codigo = ir.CUSTOMER_ID
+        LEFT JOIN viavante.catalogo cat ON cat.cnpj_cpf = clie.cnpj_cpf
+        LEFT JOIN viavante.representante r ON r.codigo = irs.id_unity
+        LEFT JOIN viavante.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+        LEFT JOIN viavante.insurance_status iss ON iss.id = irs.id_status
+        LEFT JOIN viavante.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
+        LEFT JOIN viavante.INSURANCE_VEHICLE iv ON iv.ID = irsc.ID_VEHICLE
+        LEFT JOIN viavante.TIPO_VEICULO tv ON tv.CODIGO = iv.CODE_TYPE_VEHICLE 
+        LEFT JOIN viavante.INSURANCE_REG_SET_COV_TRAILER irsct ON irsct.PARENT = irsc.ID
+        LEFT JOIN viavante.INSURANCE_TRAILER it ON it.ID = irsct.ID_TRAILER
+        LEFT JOIN viavante.insurance_trailer itt ON itt.id = irsc.ID_TRAILER
+        LEFT JOIN viavante.insurance_reg_historic irh ON irh.id_set = irs.id
+        LEFT JOIN (
+            SELECT DISTINCT irs.id as conjunto, wb.user_name as suporte
+            FROM viavante.insurance_reg_set irs 
+            INNER JOIN viavante.web_user wb ON wb.id = irs.id_user_support
+        ) as user_name ON user_name.conjunto = irs.id
+    WHERE date_cancellation IS NOT NULL
+        AND coalesce(iv.BOARD,it.BOARD,itt.BOARD) IS NOT NULL
+        AND coalesce(iv.chassi,it.chassi,itt.chassi) IS NOT NULL
+),
+
+CTE_tag AS (
+    SELECT
+        coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
+        coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
+        coalesce(iv.id,it.id,itt.id) as "id_placa",
+        coalesce(irsc.ID_VEHICLE) as "id_veiculo",
+        coalesce(irsct.ID_TRAILER) as "id_carroceria",
+        ir.id as "Matricula",
+        irs.id as "Conjunto",
+        CASE
+            WHEN cata.nome LIKE '%FTR%' THEN cata.nome
+            ELSE cata.FANTASIA
+        END as "Unidade",
+        iss.description as "Status",
+        cat.nome as "Cliente",
+        irs.data_registration  as "Data",
+        cast(irs.date_cancellation as date) as "data_cancelamento",
+        irs.user_name_cancellation as "usuario_cancelamento",
+        current_date AS "data_filtro",
+        'Viavante' as empresa,
+        irh.data_registration as "data_registro_historico",
+        -- irh.note as "Migração conferencia",
+        CASE
+            WHEN irh.note LIKE '%MIGRAÇÃO%' THEN 'SIM'
+            ELSE 'NAO'
+        END as "migracao",
+        ROW_NUMBER() OVER (PARTITION BY coalesce(iv.BOARD,it.BOARD,itt.BOARD) ORDER BY irs.data_registration DESC) as rn,
+        cast(irs.date_replaced as date) as "data_substituicao"
+    FROM tag.insurance_registration ir
+        -- joins como estavam
+        LEFT JOIN tag.insurance_reg_set irs ON irs.parent = ir.id
+        LEFT JOIN tag.cliente clie ON clie.codigo = ir.CUSTOMER_ID
+        LEFT JOIN tag.catalogo cat ON cat.cnpj_cpf = clie.cnpj_cpf
+        LEFT JOIN tag.representante r ON r.codigo = irs.id_unity
+        LEFT JOIN tag.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+        LEFT JOIN tag.insurance_status iss ON iss.id = irs.id_status
+        LEFT JOIN tag.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
+        LEFT JOIN tag.INSURANCE_VEHICLE iv ON iv.ID = irsc.ID_VEHICLE
+        LEFT JOIN tag.TIPO_VEICULO tv ON tv.CODIGO = iv.CODE_TYPE_VEHICLE 
+        LEFT JOIN tag.INSURANCE_REG_SET_COV_TRAILER irsct ON irsct.PARENT = irsc.ID
+        LEFT JOIN tag.INSURANCE_TRAILER it ON it.ID = irsct.ID_TRAILER
+        LEFT JOIN tag.insurance_trailer itt ON itt.id = irsc.ID_TRAILER
+        LEFT JOIN tag.insurance_reg_historic irh ON irh.id_set = irs.id
+        LEFT JOIN (
+            SELECT DISTINCT irs.id as conjunto, wb.user_name as suporte
+            FROM tag.insurance_reg_set irs 
+            INNER JOIN tag.web_user wb ON wb.id = irs.id_user_support
+        ) as user_name ON user_name.conjunto = irs.id
+    WHERE date_cancellation IS NOT NULL
+        AND coalesce(iv.BOARD,it.BOARD,itt.BOARD) IS NOT NULL
+        AND coalesce(iv.chassi,it.chassi,itt.chassi) IS NOT NULL
+        AND cast(irs.date_cancellation as date) >= date('2025-08-01')
+        AND cast(irs.date_replaced as date) >= DATE('2025-08-01')
 )
 
-
--------------------------------------------------------------------------------------------
-
-union all
-
--------------------------------------------------------------------------------------------
-
-
-SELECT DISTINCT
-    coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
-    coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
-    coalesce(iv.id,it.id,itt.id) as "id_placa",
-    coalesce(irsc.ID_VEHICLE) as "id_veiculo",
-    coalesce(irsct.ID_TRAILER) as "id_carroceria",
-    ir.id as "Matricula",
-    irs.id as "Conjunto",
-    cata.fantasia as "Unidade",
-    iss.description as "Status",
-    cat.nome as "Cliente",
-    irs.data_registration  as "Data",
-    cast(irs.date_cancellation as date) as "data_cancelamento",
-    irs.user_name_cancellation as "usuario_cancelamento",
-    current_date AS "data_filtro",
-    'Stcoop' as empresa
-    
-from stcoop.insurance_registration ir
-        left outer join stcoop.insurance_reg_set irs on irs.parent = ir.id
-        left outer join stcoop.cliente clie on clie.codigo = ir.CUSTOMER_ID
-        left outer join stcoop.catalogo cat on cat.cnpj_cpf = clie.cnpj_cpf
-        left outer join stcoop.representante r on r.codigo = irs.id_unity
-        left outer join stcoop.catalogo cata on cata.cnpj_cpf = r.cnpj_cpf
-        left outer join stcoop.insurance_status iss on iss.id = irs.id_status
-        left outer join stcoop.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
-        left outer join stcoop.INSURANCE_VEHICLE iv on iv.ID = irsc.ID_VEHICLE
-        left outer join stcoop.TIPO_VEICULO tv on tv.CODIGO = iv.CODE_TYPE_VEHICLE 
-        left outer join stcoop.INSURANCE_REG_SET_COV_TRAILER irsct on irsct.PARENT = irsc.ID
-        left outer join stcoop.INSURANCE_TRAILER it on it.ID = irsct.ID_TRAILER
-        left outer join stcoop.insurance_trailer itt on itt.id = irsc.ID_TRAILER
-        left outer join (
-        select distinct 
-            irs.id as conjunto,
-            wb.user_name as suporte
-    
-            from stcoop.insurance_reg_set irs 
-                inner join stcoop.web_user wb on wb.id = irs.id_user_support
-            ) as user_name on user_name.conjunto = irs.id
-            
-
-where date_cancellation is not null
-and (
-    coalesce(iv.BOARD,it.BOARD,itt.BOARD) is not null
-    and coalesce(iv.chassi,it.chassi,itt.chassi) is not null
-)
-
--------------------------------------------------------------------------------------------
-
-union all
-
--------------------------------------------------------------------------------------------
-
-
-SELECT DISTINCT
-    coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
-    coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
-    coalesce(iv.id,it.id,itt.id) as "id_placa",
-    coalesce(irsc.ID_VEHICLE) as "id_veiculo",
-    coalesce(irsct.ID_TRAILER) as "id_carroceria",
-    ir.id as "Matricula",
-    irs.id as "Conjunto",
-    cata.fantasia as "Unidade",
-    iss.description as "Status",
-    cat.nome as "Cliente",
-    irs.data_registration  as "Data",
-    cast(irs.date_cancellation as date) as "data_cancelamento",
-    irs.user_name_cancellation as "usuario_cancelamento",
-    current_date AS "data_filtro",
-    'Viavante' as empresa
-    
-from viavante.insurance_registration ir
-        left outer join viavante.insurance_reg_set irs on irs.parent = ir.id
-        left outer join viavante.cliente clie on clie.codigo = ir.CUSTOMER_ID
-        left outer join viavante.catalogo cat on cat.cnpj_cpf = clie.cnpj_cpf
-        left outer join viavante.representante r on r.codigo = irs.id_unity
-        left outer join viavante.catalogo cata on cata.cnpj_cpf = r.cnpj_cpf
-        left outer join viavante.insurance_status iss on iss.id = irs.id_status
-        left outer join viavante.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
-        left outer join viavante.INSURANCE_VEHICLE iv on iv.ID = irsc.ID_VEHICLE
-        left outer join viavante.TIPO_VEICULO tv on tv.CODIGO = iv.CODE_TYPE_VEHICLE 
-        left outer join viavante.INSURANCE_REG_SET_COV_TRAILER irsct on irsct.PARENT = irsc.ID
-        left outer join viavante.INSURANCE_TRAILER it on it.ID = irsct.ID_TRAILER
-        left outer join viavante.insurance_trailer itt on itt.id = irsc.ID_TRAILER
-        left outer join (
-        select distinct 
-            irs.id as conjunto,
-            wb.user_name as suporte
-    
-            from viavante.insurance_reg_set irs 
-                inner join viavante.web_user wb on wb.id = irs.id_user_support
-            ) as user_name on user_name.conjunto = irs.id
-            
-
-where date_cancellation is not null
-and (
-    coalesce(iv.BOARD,it.BOARD,itt.BOARD) is not null
-    and coalesce(iv.chassi,it.chassi,itt.chassi) is not null
-)
+SELECT 
+    "placa",
+    "chassi",
+    "id_placa",
+    "id_veiculo",
+    "id_carroceria",
+    "Matricula",
+    "Conjunto",
+    "Unidade",
+    "Status",
+    "Cliente",
+    "Data",
+    "data_cancelamento",
+    "usuario_cancelamento",
+    "data_filtro",
+    "empresa",
+    "data_registro_historico",
+    "migracao",
+    "data_substituicao"
+FROM (
+    SELECT * FROM CTE_segtruck WHERE rn = 1
+    UNION ALL
+    SELECT * FROM CTE_stcoop WHERE rn = 1
+    UNION ALL
+    SELECT * FROM CTE_viavante WHERE rn = 1
+    UNION ALL
+    SELECT * FROM CTE_tag WHERE rn = 1
+) AS final
--- sql/listagem_mestra.sql
+++ sql/listagem_mestra.sql
@@ -143,3 +143,56 @@ WHERE (
     coalesce(iv.BOARD,it.BOARD,itt.BOARD) is not null
     and coalesce(iv.chassi,it.chassi,itt.chassi) is not null
 )
+
+
+----------------------------------------------------------------------------------------------------
+
+union all
+
+----------------------------------------------------------------------------------------------------
+
+--EXTRAINDO DADOS COM TODAS AS PLACAS (VIAVANTE)
+            
+SELECT DISTINCT
+    irsc.id as "coverage",
+    b.DESCRIPTION as "beneficio",
+    coalesce(iv.BOARD,it.BOARD,itt.BOARD) as "placa",
+    coalesce(iv.chassi,it.chassi,itt.chassi) as "chassi",
+    coalesce(iv.id,it.id,itt.id) as "id_placa",
+    ir.id as "Matricula",
+    irs.id as "Conjunto",
+    iss.description as "status",
+    cast(coalesce(irs.date_inital_effect, date_add('day', -364, irs.date_final_effect)) as date) as "data_ativacao",
+    irs.data_registration  as "data_registro",
+    irs.id_renovated_set as "id_renovacao",
+    cast(coalesce(irsc.date_initial_effect,date_add('day', -364, irsc.date_final_effect)) as date) as "data_ativacao_beneficio",
+    CAST('1900-01-01 00:00:00' AS TIMESTAMP) 
+    + (irsc.UPDATED_AT - 599266080000000000) / 864000000000 * INTERVAL '1' DAY AS "data_atualizacao",
+    isss.description as "status_beneficio",
+    'Viavante' as empresa
+    
+from tag.insurance_registration ir
+        left outer join tag.insurance_reg_set irs on irs.parent = ir.id
+        left outer join tag.cliente clie on clie.codigo = ir.CUSTOMER_ID
+        left outer join tag.catalogo cat on cat.cnpj_cpf = clie.cnpj_cpf
+        left outer join tag.representante r on r.codigo = irs.id_unity
+        left outer join tag.catalogo cata on cata.cnpj_cpf = r.cnpj_cpf
+        left outer join tag.insurance_status iss on iss.id = irs.id_status
+        left outer join tag.INSURANCE_REG_SET_COVERAGE irsc ON irsc.PARENT = irs.ID
+        left outer join tag.INSURANCE_VEHICLE iv on iv.ID = irsc.ID_VEHICLE
+        left outer join tag.TIPO_VEICULO tv on tv.CODIGO = iv.CODE_TYPE_VEHICLE 
+        left outer join tag.INSURANCE_REG_SET_COV_TRAILER irsct on irsct.PARENT = irsc.ID
+        left outer join tag.INSURANCE_TRAILER it on it.ID = irsct.ID_TRAILER
+        left outer join tag.insurance_trailer itt on itt.id = irsc.ID_TRAILER
+        left outer join tag.insurance_status isss on isss.id = irsc.id_status
+        left outer join tag.price_list_benefits plb on plb.id = irsc.id_price_list
+        left outer join tag.type_category tc on tc.id = plb.id_type_category
+        left outer join tag.category c on c.id = tc.id_category
+        left outer join tag.benefits b on b.id = c.id_benefits
+
+WHERE (
+    coalesce(iv.BOARD,it.BOARD,itt.BOARD) is not null
+    and coalesce(iv.chassi,it.chassi,itt.chassi) is not null
+)
+
+and cast(coalesce(irsc.date_initial_effect,date_add('day', -364, irsc.date_final_effect)) as date) >= date('2025-08-01')
